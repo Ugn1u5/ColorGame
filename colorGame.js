6a36e5b925599247cf38e40631017566f195928f
Ugnius Stelingis <ugnius.stelingis@gmail.com>
2020-03-22 15:30:31 +0000

    added new color button

--- colorGame.js
+++ colorGame.js
@@ -4,20 +4,36 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function() {
+  //generate all new colors
+  colors = generateRandomColors(6);
+  // pick a new random color from array
+  pickedColor = pickColor();
+  //change colorDisplay to match picked Color
+  colorDisplay.textContent = pickedColor;
+  //change colors of squares
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+  //reset background color
+  h1.style.backgroundColor = "#232323";
+});
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
   // add initial colors to squares
-  squares[i].style.background = colors[i];
+  squares[i].style.backgroundColor = colors[i];
 
   // add click listeners to squares
   squares[i].addEventListener("click", function() {
     //grab color of clicked square
     var clickedColor = this.style.backgroundColor;
-    console.log(clickedColor, pickedColor);
     //compare color to pickedColor
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct!";
+      resetButton.textContent = "Play Again";
       changeColors(clickedColor);
       h1.style.backgroundColor = pickedColor;
     } else {
